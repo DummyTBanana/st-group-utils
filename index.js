16f610ea1d4acb6fe4c5042d730454b6b3240a83
Banana The Almighty <william.i.ney@gmail.com>
2024-08-29 15:22:39 +1000

    New Commit

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ import { groups } from "../../../group-chats.js";
 import { MacrosParser } from '../../../macros.js';
 
 //You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced,characters } from "../../../../script.js";
+import { saveSettingsDebounced,characters, setExtensionPrompt,MAX_INJECTION_DEPTH } from "../../../../script.js";
 
 // Keep track of where your extension is located, name should match repo name
 const extensionName = "st-extension-group-character-note";
+const EXTENSION_PROMPT_KEY = "ub_grouputils"
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
@@ -29,8 +30,10 @@ async function loadSettings() {
     Object.assign(extension_settings[extensionName], defaultSettings);
   }
   $("#share_character_info").prop("checked", extension_settings[extensionName].share_character_info || true).trigger("input");
+  $("#include_worldinfo").prop("checked", extension_settings[extensionName].include_worldinfo || true).trigger("input");
   $("#share_stopper").val(extension_settings[extensionName].share_stopper || '.').trigger("input");
   $("#max_share_length").val(extension_settings[extensionName].max_share_length || 200).trigger("input");
+  $("#text_depth").val(extension_settings[extensionName].text_depth || 4).trigger("input");
   $("#max_characters").val(extension_settings[extensionName].max_characters || -1).trigger("input");
 }
 
@@ -73,6 +76,41 @@ function CreateSystemNote(text) {
 }
 
 function rearrangeChat(chat){
+  try{
+    const context = getContext()
+    const group = getGroup(context.groupId)
+    let char_list = []
+    for (let i = 0; i < group.members.length; i++) {
+      const element = group.members[i];
+      const character = getCharacter(element)
+      if (character){
+        char_list.push(character)
+      }
+    }
+    if (extension_settings[extensionName].share_character_info) {
+      if (group){
+        var notes = []
+        let maxCharacters = Math.min(char_list.length,extension_settings[extensionName].max_characters == -1 && char_list.length || extension_settings[extensionName].max_characters)
+        for (let i = 0; i < maxCharacters; i++) {
+          const character = char_list[i];
+          if (character && context.name2 != character.name){
+            if (character.description.length > 0 && character.personality.length > 0){
+              const desc = getText(character.description).replaceAll("{{char}}",character.name)
+              const person = getText(character.personality).replaceAll("{{char}}",character.name)
+              notes.push(`[System Note: ${character.name} description is: ${desc} and their personality is: ${person}]`)
+            }
+          }
+        }
+        const systemNote = CreateSystemNote(notes.join("\n"))
+        setExtensionPrompt(EXTENSION_PROMPT_KEY,systemNote,1,extension_settings[extensionName].text_depth,extension_settings[extensionName].include_worldinfo)
+      }
+    }
+  }catch(e){
+    toastr.error(
+      e,
+      'An Error Occured!'
+    )
+  }
 }
 
 window['gchar_genIntercept'] = rearrangeChat;
@@ -116,6 +154,11 @@ jQuery(async () => {
     extension_settings[extensionName].share_character_info = value;
     saveSettingsDebounced();
   });
+  $("#include_worldinfo").on("input", function(event){
+    const value = Boolean($(event.target).prop("checked"));
+    extension_settings[extensionName].include_worldinfo = value;
+    saveSettingsDebounced();
+  });
   $("#share_stopper").on("input", function(event){
     const value = $(event.target).val();
     extension_settings[extensionName].share_stopper = value;
@@ -124,6 +167,11 @@ jQuery(async () => {
   $("#max_share_length").on("input", function(event){
     const value = $(event.target).val();
     extension_settings[extensionName].max_share_length = value;
+    saveSettingsDebounced();
+  });
+  $("#text_depth").on("input", function(event){
+    const value = $(event.target).val();
+    extension_settings[extensionName].text_depth = value;
     saveSettingsDebounced();
   });
   $("#max_characters").on("input", function(event){
