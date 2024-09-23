49b675bc40fd27e860c6e7e260a9b54d3f1a4965
Banana The Almighty <william.i.ney@gmail.com>
2024-09-23 19:19:18 +1000

    Auto Commit

--- index.js
+++ index.js
@@ -248,6 +248,7 @@ jQuery(async () => {
       extension_settings[extensionName]['character_data'] = {}
     }
     extension_settings[extensionName]['character_data'][character.name] = value
+    console.log(extension_settings[extensionName])
     saveSettingsDebounced();
   })
 
